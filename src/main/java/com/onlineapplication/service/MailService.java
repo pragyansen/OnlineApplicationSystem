e812b71f74bf81442c86d067764762f516fc1e34
Pragyan <Pragyan-PC>
2016-02-21 16:36:09 +0530

    Velocity Integration completed. HTML is mail is being generated

--- src/main/java/com/onlineapplication/service/MailService.java
+++ src/main/java/com/onlineapplication/service/MailService.java
@@ -1,15 +1,21 @@
 package com.onlineapplication.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.mail.Message;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import com.onlineapplication.model.Student;
 import com.onlineapplication.mvc.controller.SecurityController;
@@ -20,22 +26,28 @@ public class MailService {
 	@Autowired
 	private JavaMailSender mailSender;
 	
+	@Autowired
+	private VelocityEngine velocityEngine;
+	
 	private static final Logger logger = Logger.getLogger(SecurityController.class);
 	
     public void sendMail(final Student student) {
     	
     	  MimeMessagePreparator preparator = new MimeMessagePreparator() {
 
-              public void prepare(MimeMessage mimeMessage) throws Exception {
+              @SuppressWarnings("deprecation")
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+            	  MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+            	  message.setTo(student.getEmail());
+            	  message.setFrom(new InternetAddress("pragyan.sen@gmail.com"));
+            	  message.setSubject("Registration Success");
 
-                  mimeMessage.setRecipient(Message.RecipientType.TO,
-                          new InternetAddress(student.getEmail()));
-                  mimeMessage.setFrom(new InternetAddress("pragyan.sen@gmail.com"));
-                  mimeMessage.setSubject("Registration Success");
-                  mimeMessage.setText("Dear " + student.getName() + ", \n" +
-                		  "Your password is "+ student.getPassword()
-                		  
-                		  );
+                  
+                  Map<String,Object> model = new HashMap<String, Object>();
+                  model.put("student", student);
+                  String text = VelocityEngineUtils.mergeTemplateIntoString(
+                          velocityEngine, "com/onlineapplication/service/registration-confirmation.vm", model);
+                  message.setText(text, true);
               }
           };
 
