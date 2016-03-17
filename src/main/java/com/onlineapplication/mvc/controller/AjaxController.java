a4833be7e5c35d06637310a251320748b1dbef54
Subhendu <Subhendu@SUBHENDU-PC>
2016-03-18 01:52:29 +0530

    Final commit after fixing conflict

--- src/main/java/com/onlineapplication/mvc/controller/AjaxController.java
+++ src/main/java/com/onlineapplication/mvc/controller/AjaxController.java
@@ -7,13 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-<<<<<<< HEAD
 import com.onlineapplication.dao.AdminDashBoardDao;
-import com.onlineapplication.mvc.bean.EducationalDetails;
-=======
 import com.onlineapplication.model.EducationalDetails;
-import com.onlineapplication.model.Subject;
->>>>>>> branch 'master' of https://subhendu_pal@bitbucket.org/pragyanteam/onlineapplication.git
 
 @Controller
 @RequestMapping(value = "/ajax")
