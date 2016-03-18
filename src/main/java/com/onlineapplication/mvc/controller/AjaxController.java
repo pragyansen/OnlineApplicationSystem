d4c53ac9cd7d513c7a49bd7815a492a4469ae9f2
AG <arunabha.gw@outlook.com>
2016-03-18 22:46:57 +0530

    Ajax Controller Fix

--- src/main/java/com/onlineapplication/mvc/controller/AjaxController.java
+++ src/main/java/com/onlineapplication/mvc/controller/AjaxController.java
@@ -1,25 +1,22 @@
 package com.onlineapplication.mvc.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.onlineapplication.dao.AdminDashBoardDao;
-import com.onlineapplication.model.EducationalDetails;
 
 @Controller
 @RequestMapping(value = "/ajax")
 public class AjaxController {
 
-	@ResponseBody
+	/*@ResponseBody
 	@RequestMapping(value = "/fetchSubjectList")
 	public Object fetchSubjectList(){
 		EducationalDetails educationalDetails = new EducationalDetails();
 		return educationalDetails.getSubjects();
-	}
+	}*/
 	
 	
 	@ResponseBody
